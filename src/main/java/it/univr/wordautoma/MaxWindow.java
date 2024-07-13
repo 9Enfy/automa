61214e907d9db7ea4267554a0a60f6cd836ae888
francesco <francycorato03@gmail.com>
2024-07-13 16:55:11 +0200

    Aggiunti commenti Sistemato spacing e pulsanti finestre di dialogo

--- src/main/java/it/univr/wordautoma/MaxWindow.java
+++ src/main/java/it/univr/wordautoma/MaxWindow.java
@@ -2,20 +2,39 @@ package it.univr.wordautoma;
 
 import java.awt.*;
 
+/**
+ * Classe per gestire la grandezza della finestra in base allo spazio disponibile nello schermo
+ * (tiene conto anche delle barre laterali)
+ */
 public class MaxWindow {
     private static final MaxWindow instance = new MaxWindow();
     private final int width;
     private final int height;
 
+    /**
+     * Costruttore che imposta la larghezza e l'altezza della finestra
+     */
     private MaxWindow() {
         Rectangle screenSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
         width = screenSize.width;
         height = screenSize.height;
     }
 
+    /**
+     * Metodo che ritorna l'istanza della classe
+     * @return Istanza della classe
+     */
     public static MaxWindow getInstance() { return instance; }
 
+    /**
+     * Metodo che ritorna la larghezza della finestra
+     * @return Larghezza della finestra
+     */
     public int getWidth() { return width; }
 
+    /**
+     * Metodo che ritorna l'altezza della finestra
+     * @return Altezza della finestra
+     */
     public int getHeight() { return height; }
 }
--- src/main/java/it/univr/wordautoma/Message.java
+++ src/main/java/it/univr/wordautoma/Message.java
@@ -2,14 +2,26 @@ package it.univr.wordautoma;
 
 import java.util.List;
 
+/**
+ * Classe che gestisce il messaggio da visualizzare negli alert
+ */
 public class Message {
     private String msg = "";
     private String sequence = "";
 
+    /**
+     * Metodo per impostare la frase risultato
+     * @param msg
+     */
     public void setMessage(String msg){
         this.msg = msg;
     }
 
+    /**
+     * Metodo per impostare la sequenza di passi del risultato
+     * @param nodes
+     * @param weigth
+     */
     public void setSequence(List<String> nodes,List<String> weigth){
         if(nodes == null) return;
 
@@ -19,6 +31,10 @@ public class Message {
         sequence+= nodes.get(nodes.size()-1);
     }
 
+    /**
+     * Metodo per ritornare il messaggio da visualizzare nell'alert
+     * @return Messaggio da stampare nell'alert
+     */
     public String getResult(){
         return msg + sequence;
     }
